83fd593e1f892de4816a01bc4d8dfe342b30b1b7
Andrea Thniah <andreathniah@gmail.com>
2018-08-12 23:27:28 +0800

    added mobile support for fanfiction.net

---
+++ js/asianFanfiction.js
@@ -0,0 +1,205 @@
+// https://www.asianfanfics.com/story/view/1192719/11/yes-ma-am-friendship-romance-teacher-school-exo-chen-firstfrost (members, subscribers only)
+
+$(function() {
+  var requestedURL = localStorage.getItem("urlInput");
+  var submitStatus;
+  loadChapterNo(requestedURL, function(databox) {
+    m18Status = databox.getElementById("is_of_age"); //M18 tag
+
+    console.log("m18Status = " + m18Status);
+    if (m18Status != null) {
+      databox.getElementsByTagName("form")[1].submit();
+      // maybe need to refresh the page?
+    }
+  });
+
+  loadChapterNo(requestedURL, function(databox) {
+    executeScrapping(requestedURL, databox);
+  });
+});
+
+function executeScrapping(requestedURL, databox) {
+  var htmlArray = [];
+  var counter = 0; // need to consider the overview pagge
+  var chapterNo = getChapterNo(databox);
+  console.log("Total no of chapters: " + chapterNo);
+  // from foreword to last chapter
+  for (i=0; i<=chapterNo; i++) {
+      var updatedURL = updateURL(requestedURL, i);
+      htmltoArray(updatedURL, htmlArray, i, function(resultbox) {
+        progressMsg = "fetching " + (counter++) + " of " + chapterNo;
+        document.getElementById("progress").innerHTML = progressMsg;
+
+        if (counter > chapterNo) {
+          for (j=0; j<=chapterNo; j++) {
+            loadStoryContent(resultbox[j], j);
+          }
+          document.getElementById("progress").outerHTML = "";
+          document.getElementById("loading").outerHTML = "";
+        }
+      });
+  }
+  var hello = document.getElementsByClassName("click-to-read-full");
+  console.log(hello);
+  document.getElementById("storyContent").style.visibility = "visible";
+}
+
+
+// XMLHttpRequest to grab URL's HTML information
+function loadStoryContent(htmlContent, index) {
+  // append the summary
+  if (index == 0) {
+    var summaryDiv = document.getElementById("summary-container");
+    var summaryContent = htmlContent.getElementById("bodyText");
+    summaryDiv.appendChild(summaryContent);
+  }
+  else {
+    var storyDiv = document.getElementById("story-container");
+    var storyContent = htmlContent.getElementById("user-submitted-body");
+    var chapterSelect = htmlContent.getElementsByClassName("chapter-nav");
+
+    // get drop-down-list text and value
+    var selectedText = chapterSelect[0].options[chapterSelect[0].selectedIndex].text;
+    var selectedValue = chapterSelect[0].options[chapterSelect[0].selectedIndex].value;
+    console.log(selectedText);
+
+    var titleNode = document.createElement("p");
+    titleNode.style.cssText = "font-weight: bold";
+    titleNode.append(selectedText); // append because its not a node, its a normal string
+
+    var storyNode = document.createElement("div");
+    storyNode.id = "chapter-" + selectedValue;
+    storyNode.className = "row twelve columns chapter-container";
+
+    storyNode.appendChild(titleNode);
+    storyNode.appendChild(storyContent);
+    storyDiv.appendChild(storyNode);
+  }
+}
+
+function htmltoArray(requestedURL, htmlArray, index, callback) {
+  var xhttp = new XMLHttpRequest();
+  var proxyURL = getProxyURL(requestedURL);
+
+  xhttp.open("GET", proxyURL, true);
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200 && callback) {
+      var httpDoc = domParse(xhttp.responseText);
+      htmlArray[index] = httpDoc;
+      callback(htmlArray);
+    }
+  }
+  xhttp.send();
+}
+
+// XMLHttpRequest callback to grab total no of chapters
+function loadChapterNo(requestedURL, callback) {
+  var xhttp = new XMLHttpRequest();
+  var proxyURL = getProxyURL(requestedURL);
+
+  xhttp.open("GET", proxyURL, true);
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200 && callback) {
+      var httpDoc = domParse(xhttp.responseText);
+      callback(httpDoc);
+    }
+  }
+  xhttp.send();
+}
+
+// isolate nav-bar to get the last digit of the chapter
+function getChapterNo(httpDoc) {
+  var chapterNo, lastValue, lastChapter;
+
+  // chapterNo is a HTMLCollection
+  chapterNo = httpDoc.getElementsByClassName("chapter-nav");
+  lastValue = chapterNo[0].options[chapterNo[0].options.length - 1].value;
+  lastChapter = lastValue.split("/");
+
+  return lastChapter[4];
+}
+
+// update requestedURL to the correct URL base on its chapter
+function updateURL(requestedURL, chapterIndex) {
+  var updatedURL, part1, part2, urlArray, digitInChapter;
+
+  urlArray = requestedURL.split("/");
+
+  if (urlArray.length < 8) { // starting requestedURL is a foreword
+    part1 = requestedURL.substr(0, requestedURL.lastIndexOf("/"));
+    part2 = requestedURL.substr(requestedURL.lastIndexOf("/"));
+
+    if (chapterIndex == 0) {
+      updatedURL = part1 + part2;
+    }
+    else {
+      updatedURL = part1 + "/" + chapterIndex + part2;
+    }
+  }
+  else { // starting requestedURL is a chapter page
+    if (chapterIndex == 0) {
+        // get rid of chapter and /
+        digitInChapter = urlArray[6].toString().length + 1;
+        part1 = requestedURL.substr(0, requestedURL.lastIndexOf("/") - digitInChapter);
+        part2 = requestedURL.substr(requestedURL.lastIndexOf("/"));
+        updatedURL = part1 + part2;
+    }
+    else {
+        digitInChapter = urlArray[6].toString().length;
+        part1 = requestedURL.substr(0, requestedURL.lastIndexOf("/") - digitInChapter);
+        part2 = requestedURL.substr(requestedURL.lastIndexOf("/"));
+        updatedURL = part1 + chapterIndex + part2;
+    }
+  }
+  return updatedURL;
+}
+
+// facilate node.js CORS-anywhere npm
+function getProxyURL(requestedURL) {
+  var proxyURL, urlArray, totalLength, part1, part2;
+
+  urlArray = requestedURL.split("/");
+  totalLength = (urlArray[0] + urlArray[1]).length + 6;
+  part1 = "https://cors-anywhere.herokuapp.com/";
+  part2 = requestedURL.substr(totalLength, requestedURL.lastIndexOf("/"));
+  proxyURL = part1 + part2;
+
+  return proxyURL;
+}
+
+// convert XML data into a HTMLDocument
+function domParse(data) {
+  var parser = new DOMParser();
+  var httpDoc = parser.parseFromString(data, "text/html");
+
+  return httpDoc;
+}
+
+function sendToServer() {
+  console.log("POST request sent to server");
+  var oReq = new XMLHttpRequest();
+  oReq.open("POST", "/topdf", true);
+  //Send the proper header information along with the request
+  oReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  oReq.responseType = "blob";
+
+  oReq.onload = function(oEvent) {
+    var blob = oReq.response;
+    //Create a link element, hide it, direct
+    //it towards the blob, and then 'click' it programatically
+    let a = document.createElement("a");
+    a.style = "display: none";
+    document.body.appendChild(a);
+    //Create a DOMString representing the blob
+    //and point the link element towards it
+    let url = window.URL.createObjectURL(blob);
+    a.href = url;
+    a.download = 'myFile.pdf';
+    //programatically click the link to trigger the download
+    a.click();
+    //release the reference to the file by revoking the Object URL
+    window.URL.revokeObjectURL(url);
+    console.log("This will take a while...");
+  };
+  oReq.send(document.documentElement.outerHTML);
+}
---
+++ js/fanfictionNet.js
@@ -0,0 +1,237 @@
+$(function() {
+	var requestedURL, updatedURL, progressMsg;
+	var htmlArray = [];
+	var counter = 0;
+
+	requestedURL = localStorage.getItem("urlInput");
+
+	loadChapterNo(requestedURL, function(chapterNo) {
+		for (i = 1; i <= chapterNo; i++) {
+			updatedURL = updateURL(requestedURL, i);
+			htmlToArray(updatedURL, htmlArray, i, function(databox) {
+				// progress index
+				progressMsg =
+					"Fetching " + counter++ + " of " + chapterNo + " chapters.";
+				document.getElementById("progress").innerHTML = progressMsg;
+
+				if (counter == chapterNo) {
+					for (j = 1; j <= chapterNo; j++) {
+						loadStoryContent(databox[j], j);
+					}
+					$("div[style]").removeAttr("style");
+					document.getElementById("progress").outerHTML = "";
+					document.getElementById("loading").outerHTML = "";
+				}
+			});
+		}
+		document.getElementById("storyContent").style.visibility = "visible";
+	});
+});
+
+function loadStoryContent(htmlContent, index) {
+	if (index == 1) {
+		var summaryDiv = document.getElementById("summary-container");
+		var summaryContent = htmlContent.querySelector("#content > div").innerText;
+
+		var summaryTitleNode = document.createElement("header");
+		var summaryNode = document.createElement("p");
+
+		summaryTitleNode.style.cssText = "font-weight: bold; ";
+		summaryTitleNode.append(summaryContent);
+		summaryDiv.appendChild(summaryTitleNode);
+	}
+
+	var storyDiv = document.getElementById("story-container");
+	var storyContent = htmlContent.getElementById("storycontent");
+	var chapterSelect = htmlContent
+		.getElementById("content")
+		.innerHTML.split(/\r?\n/);
+	var selectedText = chapterSelect[chapterSelect.length - 1].slice(0, -4);
+
+	var titleNode = document.createElement("p");
+	titleNode.style.cssText = "font-weight: bold";
+	titleNode.append(selectedText); // append because its not a node, its a normal string
+
+	var storyNode = document.createElement("div");
+	storyNode.id = "chapter-" + selectedText;
+	storyNode.className = "row twelve columns chapter-container";
+
+	storyNode.appendChild(titleNode);
+	storyNode.appendChild(storyContent);
+	storyDiv.appendChild(storyNode);
+}
+
+// XMLHttpRequest to grab URL's HTML information
+function htmlToArray(requestedURL, htmlArray, index, callback) {
+	var cors_api_url = "http://cors-anywhere.herokuapp.com/";
+	const proxyURL = getProxyURL(requestedURL);
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", cors_api_url + proxyURL);
+	xhttp.onload = function() {
+		const parser = new DOMParser();
+		const httpDoc = parser.parseFromString(xhttp.responseText, "text/html");
+		htmlArray[index] = httpDoc;
+		callback(htmlArray);
+	};
+	xhttp.send();
+}
+
+// XMLHttpRequest callback to grab total no of chapters
+function loadChapterNo(requestedURL, callback) {
+	var cors_api_url = "http://cors-anywhere.herokuapp.com/";
+	const proxyURL = getProxyURL(requestedURL);
+	console.log(proxyURL);
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("GET", cors_api_url + proxyURL);
+	xhttp.onload = function() {
+		const parser = new DOMParser();
+		const httpDoc = parser.parseFromString(xhttp.responseText, "text/html");
+		var chapterNo = getChapterNo(httpDoc);
+		console.log("Total number of chapters: " + chapterNo);
+		callback(chapterNo);
+	};
+	xhttp.send();
+}
+
+// isolate nav-bar to get the last digit of the chapter
+function getChapterNo(httpDoc) {
+	var chapterNo, lastValue;
+
+	lastValue = httpDoc.querySelector("#top > div:nth-child(7) > a:nth-child(1)")
+		.innerHTML;
+
+	return lastValue;
+}
+
+// generate PDF with page breaks
+function generatePDF() {
+	// https://plnkr.co/edit/64KOSxMgDWfRUfg2bxfo?p=preview
+	var pdf = new jsPDF();
+	var pdfName = "pagebreaks.pdf";
+	var options = { width: 170, pagesplit: true };
+
+	var $div = $(".chapter-container"); // gets all div of the same name
+	var noRecursionNeeded = $div.length - 1;
+	var currentRecursion = 0;
+
+	function recursiveAddHtmlAndSave(currentRecursion, totalRecursions) {
+		//Once we have done all the divs save the pdf
+		if (currentRecursion == totalRecursions) {
+			pdf.save(pdfName);
+		} else {
+			pdf.addPage();
+			pdf.fromHTML(
+				$(".chapter-container")[currentRecursion],
+				15,
+				15,
+				options,
+				function() {
+					console.log("Appending - chapter " + currentRecursion);
+					currentRecursion++;
+					recursiveAddHtmlAndSave(currentRecursion, totalRecursions);
+				}
+			);
+		}
+	}
+
+	pdf.fromHTML(
+		$(".summary-container")[currentRecursion],
+		15,
+		15,
+		options,
+		function() {
+			console.log("Appending - summary");
+			recursiveAddHtmlAndSave(currentRecursion, noRecursionNeeded);
+		}
+	);
+}
+
+// generate PDF without page breaks
+function convertToPDF() {
+	// known issues: https://github.com/MrRio/jsPDF/issues/648
+	var doc = new jsPDF();
+
+	doc.fromHTML($("#storyContent")[0], 15, 15, { width: 170 }, function() {
+		doc.save("continous.pdf");
+	});
+}
+
+function validateURL(requestedURL) {
+	const desktopLink = requestedURL.includes("https://www.fanfiction.net/s/");
+	const mobileLink = requestedURL.includes("https://m.fanfiction.net/s/");
+
+	if (desktopLink) {
+		return requestedURL.replace(
+			"https://www.fanfiction.net/s/",
+			"m.fanfiction.net/s/"
+		);
+	} else if (mobileLink) {
+		return requestedURL.replace(
+			"https://m.fanfiction.net/s/",
+			"m.fanfiction.net/s/"
+		);
+	} else {
+		return false;
+	}
+}
+function getProxyURL(requestedURL) {
+	var proxyURL = "http://cors-anywhere.herokuapp.com/";
+	var validation = validateURL(requestedURL);
+	if (validation !== false) {
+		return proxyURL + validation;
+	}
+}
+
+function updateURL(requestedURL, chapterIndex) {
+	var updatedURL, part1, part2, urlArray, digitInChapter;
+
+	urlArray = requestedURL.split("/");
+	digitInChapter = urlArray[5].toString().length;
+
+	part1 = requestedURL.substr(
+		0,
+		requestedURL.lastIndexOf("/") - digitInChapter
+	);
+	part2 = requestedURL.substr(requestedURL.lastIndexOf("/"));
+	updatedURL = part1 + chapterIndex + part2;
+
+	return updatedURL;
+}
+
+function domParse(data) {
+	var parser = new DOMParser();
+	var httpDoc = parser.parseFromString(data, "text/html");
+
+	return httpDoc;
+}
+
+function sendToServer() {
+	console.log("POST request sent to server");
+	var oReq = new XMLHttpRequest();
+	oReq.open("POST", "/topdf", true);
+	//Send the proper header information along with the request
+	oReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	oReq.responseType = "blob";
+
+	oReq.onload = function(oEvent) {
+		var blob = oReq.response;
+		//Create a link element, hide it, direct
+		//it towards the blob, and then 'click' it programatically
+		let a = document.createElement("a");
+		a.style = "display: none";
+		document.body.appendChild(a);
+		//Create a DOMString representing the blob
+		//and point the link element towards it
+		let url = window.URL.createObjectURL(blob);
+		a.href = url;
+		a.download = "myFile.pdf";
+		//programatically click the link to trigger the download
+		a.click();
+		//release the reference to the file by revoking the Object URL
+		window.URL.revokeObjectURL(url);
+		console.log("This will take a while...");
+	};
+	oReq.send(document.documentElement.outerHTML);
+}
